7362a3c90df8615cc64ce3bc78c41c65ee9a8b5e
Enrique Hernandez Montiel <enriquehmsb@gmail.comgit congig --global -egit config --global user.email enriquehmsb@gmail.com>
2024-04-20 15:19:43 -0600

    dee

--- main.js
+++ main.js
@@ -1,19 +1,48 @@
 function mostrarContador() {
-  fetch("https://testcookies.fly.dev/api/v1", {
+  fetch("http://localhost:8081/api/v1", {
     method: "POST",
     credentials: "include",
     headers: {
       "Content-Type": "application/json",
     },
+    body: JSON.stringify({ tokenRes: 22222 }),
   })
     .then((res) => res.json())
     .then((json) => {
       inventarioVinateria = json;
       console.log(inventarioVinateria);
+      let represent = inventarioVinateria;
       let html = "";
       html += `
 
-      <h1> vistas: ${inventarioVinateria.vistas} </h1>
+      <h1> vistas: ${represent} </h1>
+      
+      `;
+      document.getElementById("counter").innerHTML = html;
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+}
+function verUser() {
+  fetch("http://localhost:8081/", {
+    method: "GET",
+    credentials: "include",
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      let represent;
+      inventarioVinateria = json.cookie;
+      if (inventarioVinateria.user) {
+        represent = inventarioVinateria.user;
+      } else {
+        represent = "nadie";
+      }
+      console.log(inventarioVinateria);
+      let html = "";
+      html += `
+
+      <h1> vistas: ${represent} </h1>
       
       `;
       document.getElementById("counter").innerHTML = html;
